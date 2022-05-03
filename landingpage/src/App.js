c7d2560ecc0bcd6346e5fa4cb53d29475baa582f
diya <divya5320.doodleblue@gmail.com>
2022-05-03 15:30:37 +0530

    'finalassessment'

---
+++ landingpage/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import Component1 from './Component1'
+import Component from './Component'
+
+
+
+function App() {
+  return (
+    <div>
+      
+      {/* <Component1/> */}
+      <Component/>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ landingpage/src/Component.js
@@ -0,0 +1,400 @@
+import React from 'react'
+import './component.css'
+import arrowdown from './assets/arrow down.png'
+import profileclose2 from './assets/profile_close_2.png'
+import ellipse from './assets/Ellipse.png'
+import vector1 from './assets/Vector1.png'
+import vector2 from './assets/Vector2.png'
+import vector3 from './assets/Vector2.png'
+import search from './assets/search-icon.png'
+import rectangle383 from './assets/Rectangle 383.png'
+import vector40 from './assets/Vector 40 (Stroke) (1).png'
+import promotionimage from './assets/Promotion Image.png'
+import bag from './assets/image Product.png'
+import one from './assets/$299,43.png'
+import shoe from './assets/shoe.png'
+import redshoe from './assets/redshoe.png'
+import shoe3 from './assets/shoe3.png'
+import bag3 from './assets/bag3.png'
+import bag4 from './assets/bag4.png'
+import overlay from './assets/overlay.png'
+import hearts from './assets/hearts.png'
+import cart_2 from './assets/cart_2.png'
+import shoe4 from './assets/shoe4.png'
+import starcopy1 from './assets/star copy 1.png'
+import starcopy2 from './assets/star copy 1.png'
+import starcopy3 from './assets/star copy 1.png'
+import starcopy4 from './assets/star copy 1.png'
+import starcopys1 from './assets/star copys 1.png'
+import starcopy5 from './assets/star copy 1.png'
+import starcopy6 from './assets/star copy 1.png'
+import starcopy7 from './assets/star copy 1.png'
+import starcopy8 from './assets/star copy 1.png'
+import starcopys2 from './assets/star copys 1.png'
+import starcopy9 from './assets/star copy 1.png'
+import starcopy10 from './assets/star copy 1.png'
+import starcopy11 from './assets/star copy 1.png'
+import starcopy12 from './assets/star copy 1.png'
+import starcopys3 from './assets/star copys 1.png'
+import starcopy13 from './assets/star copy 1.png'
+import starcopy14 from './assets/star copy 1.png'
+import starcopy15 from './assets/star copy 1.png'
+import starcopy16 from './assets/star copy 1.png'
+import starcopys4 from './assets/star copys 1.png'
+import shoe5 from './assets/shoe5.png'
+import shoe6 from './assets/shoe6.png'
+import shoe7 from './assets/shoe7.png'
+import shoe8 from './assets/shoe8.png'
+import starcopy17 from './assets/star copy 1.png'
+import starcopy18 from './assets/star copy 1.png'
+import starcopy19 from './assets/star copy 1.png'
+import starcopy20 from './assets/star copy 1.png'
+import starcopys5 from './assets/star copys 1.png'
+import starcopy21 from './assets/star copy 1.png'
+import starcopy22 from './assets/star copy 1.png'
+import starcopy23 from './assets/star copy 1.png'
+import starcopy24 from './assets/star copy 1.png'
+import starcopys6 from './assets/star copys 1.png'
+import starcopy25 from './assets/star copy 1.png'
+import starcopy26 from './assets/star copy 1.png'
+import starcopy27 from './assets/star copy 1.png'
+import starcopy28 from './assets/star copy 1.png'
+import starcopys7 from './assets/star copys 1.png'
+import starcopy29 from './assets/star copy 1.png'
+import starcopy30 from './assets/star copy 1.png'
+import starcopy31 from './assets/star copy 1.png'
+import starcopy32 from './assets/star copy 1.png'
+import starcopys8 from './assets/star copys 1.png'
+import rectangle14 from './assets/Rectangle 14.png'
+import rectangle1copy10 from './assets/Rectangle 1 copy 10.png'
+import shoe9 from './assets/shoe9.png'
+import whiteline from './assets/whiteline.png'
+import shipping from './assets/shipping.png'
+import path1 from './assets/Path 1.png'
+import path2 from './assets/Path 2.png'
+import path3 from './assets/Path 3.png'
+import path4 from './assets/Path 4.png'
+import emblem from './assets/emblem.png'
+import figma from './assets/figma.png'
+import kronos from './assets/kronos.png'
+import shoe10 from './assets/shoe10.png'
+import shoe11 from './assets/shoe10.png'
+import shoe12 from './assets/shoe10.png'
+import starcopy33 from './assets/star copy 1.png'
+import starcopy34 from './assets/star copy 1.png'
+import starcopy35 from './assets/star copy 1.png'
+import starcopy36 from './assets/star copy 1.png'
+import starcopys9 from './assets/star copys 1.png'
+import starcopy37 from './assets/star copy 1.png'
+import starcopy38 from './assets/star copy 1.png'
+import starcopy39 from './assets/star copy 1.png'
+import starcopy40 from './assets/star copy 1.png'
+import starcopys10 from './assets/star copys 1.png'
+import starcopy41 from './assets/star copy 1.png'
+import starcopy42 from './assets/star copy 1.png'
+import starcopy43 from './assets/star copy 1.png'
+import starcopy44 from './assets/star copy 1.png'
+import starcopys11 from './assets/star copys 1.png'
+import line1 from './assets/Line 1.png'
+import line2 from './assets/Line 1.png'
+import line3 from './assets/Line 1.png'
+import bluebox from './assets/bluebox.png'
+import roundblue from './assets/roundblue.png'
+import rectangle437 from './assets/Rectangle 437.png'
+import rectangle3831 from './assets/Rectangle 383.png'
+import vector401 from './assets/Vector 40 (Stroke) (1).png'
+import facebook from './assets/facebook.png'
+import twitter from './assets/twitter.png'
+import whitelength from './assets/whitelength.png'
+import Western_union from './assets/Western-union.png'
+import Group19 from './assets/Group 19.png'
+import Paypal from './assets/Paypal.png'
+import visa1 from './assets/visa1.png'
+import visa2 from './assets/visa2.png'
+import visa3 from './assets/visa3.png'
+
+
+
+function Component() {
+  return (
+    <div>
+      
+        <div className='EN'>EN</div>
+        <img className='arrowdown1' src={arrowdown} alt='arrowdown'/>
+        <div className='usd'>USD</div>
+        <img className='arrowdown2'src={arrowdown} alt='arrowndown'/>
+        <img className='profileclose2' src={profileclose2} alt='profile'/>
+        <div className='myprofile'>MY profile</div>
+        <img className='ellipse' src={ellipse} alt='notification mark'/>
+        
+
+        <img  className='vector1'src={vector1} alt='vector1'/>
+                  <img className='vector2'src={vector2} alt='vector2'/>
+                  <img className='vector3'src={vector3} alt='vector3'/>
+                  <div className='items'> Items</div>
+                  <div className='dollar'>$0.00</div>
+                  <img className='search'src={search} alt='searchicon'/>
+                  <div className='dho'>2</div>
+                  
+        <div className='divider'/>
+        <img className='rectangle383'src={rectangle383} alt='box'/>
+        <img className='vector40'src={vector40} alt='vector40'/>
+        <div className='Comm'>E-Comm</div>
+        <div className='home'>HOME</div>
+        <div className='bags'>BAGS</div>
+        <div className='sneakers'>SNEAKERS</div>
+        <div className='belt'>BELT</div>
+        <div className='contact'>CONTACT</div>
+        <img  className='promotionimage'src={promotionimage} alt='promoimage'/>
+        <div className='super'>Super Flash Sale <br/>50% Off</div>
+        <img className='bag'src={bag} alt='bag'/>
+        <div className='maxi'>FS - QUILTED MAXI <br/>CROSS BAG</div>
+        <div className='five'>$534,33</div>
+        <div className='off'>24% Off</div>
+        <img  className='two' src={one} alt='one'/>
+        <img  className='shoe'src={shoe} alt='shoe'/>
+        <div className='nikeair'>FS - Nike Air<br/> Max 270 React...</div>
+        <div className='five1'>$534,33</div>
+        <div className='off1'>24% Off</div>
+        <img  className='two1' src={one} alt='one'/>
+        <img  className='redshoe'src={redshoe} alt='shoe'/>
+        <div className='nikeair1'>FS - Nike Air Max 270 React...</div>
+        <div className='five2'>$534,33</div>
+        <div className='off2'>24% Off</div>
+        <img  className='two2' src={one} alt='one'/>
+        <div className='bestseller'>BEST SELLER</div>
+        <div className='all'>All</div>
+        <div className='bag1'>Bags</div>
+        <div className='neak'>Sneakers</div>
+        <div className='belt1'>belt</div>
+        <div className='sun'>Sunglasses</div>
+        <img className='shoe3'src={shoe3} alt='shoe3'/>
+        <div className='rectangle20'/>
+        <div className='hot'>HOT</div>
+        <img className='bag3'src={bag3} alt='bag3'/>
+        <img className='bag4'src={bag4} alt='bag4'/>
+        <img className='overlay'src={overlay} alt='overlay'/>
+        <img className='hearts'src={hearts} alt='hearts'/>
+        <img className='cart_2'src={cart_2} alt='cart_2'/>
+        <img className='shoe4'src={shoe4} alt='shoe4'/>
+        <div className='air1'>Nike Air Max 270 React</div>
+        <div className='air2'>Nike Air Max 270 React</div>
+        <div className='air3'>Nike Air Max 270 React</div>
+        <div className='air4'>Nike Air Max 270 React</div>
+        <img className='starcopy1' src={starcopy1} alt='starcopy1'/>
+        <img className='starcopy2' src={starcopy2} alt='starcopy2'/>
+        <img className='starcopy3' src={starcopy3} alt='starcopy3'/>
+        <img className='starcopy4' src={starcopy4} alt='starcopy1'/>
+        <img className='starcopys1' src={starcopys1} alt='starcopys1'/>
+        <img className='starcopy5' src={starcopy5} alt='starcopy1'/>
+        <img className='starcopy6' src={starcopy6} alt='starcopy1'/>
+        <img className='starcopy7' src={starcopy7} alt='starcopy1'/>
+        <img className='starcopy8' src={starcopy8} alt='starcopy1'/>
+        <img className='starcopys2' src={starcopys2} alt='starcopys1'/>
+        <img className='starcopy9' src={starcopy9} alt='starcopy1'/>
+        <img className='starcopy10' src={starcopy10} alt='starcopy1'/>
+        <img className='starcopy11' src={starcopy11} alt='starcopy1'/>
+        <img className='starcopy12' src={starcopy12} alt='starcopy1'/>
+        <img className='starcopys3' src={starcopys3} alt='starcopy1'/>
+        <img className='starcopy13' src={starcopy13} alt='starcopy1'/>
+        <img className='starcopy14' src={starcopy14} alt='starcopy1'/>
+        <img className='starcopy15' src={starcopy15} alt='starcopy1'/>
+        <img className='starcopy16' src={starcopy16} alt='starcopy1'/>
+        <img className='starcopys4' src={starcopys4} alt='starcopy1'/>
+        <div className='n1'>$299,43</div>
+        <div className='f1'>$534,33</div>
+        <div className='o1'>24% Off</div>
+
+        <div className='n2'>$299,43</div>
+        <div className='f2'>$534,33</div>
+        <div className='o2'>24% Off</div>
+        
+        <div className='n3'>$299,43</div>
+        <div className='f3'>$534,33</div>
+        <div className='o3'>24% Off</div>
+        
+        <div className='n4'>$299,43</div>
+        <div className='f4'>$534,33</div>
+        <div className='o4'>24% Off</div>
+
+        <img className='shoe5'src={shoe5} alt='shoe5'/>
+        <img className='shoe6'src={shoe6} alt='shoe6'/>
+        <img className='shoe7'src={shoe7} alt='shoe7'/>
+        <img className='shoe8'src={shoe8} alt='shoe8'/>
+
+        <div className='air5'>Nike Air Max 270 React</div>
+        <div className='air6'>Nike Air Max 270 React</div>
+        <div className='air7'>Nike Air Max 270 React</div>
+        <div className='air8'>Nike Air Max 270 React</div>
+        
+        <img className='starcopy17' src={starcopy17} alt='starcopy1'/>
+        <img className='starcopy18' src={starcopy18} alt='starcopy1'/>
+        <img className='starcopy19' src={starcopy19} alt='starcopy1'/>
+        <img className='starcopy20' src={starcopy20} alt='starcopy1'/>
+        <img className='starcopys5' src={starcopys5} alt='starcopy1'/>
+        <img className='starcopy21' src={starcopy21} alt='starcopy1'/>
+        <img className='starcopy22' src={starcopy22} alt='starcopy1'/>
+        <img className='starcopy23' src={starcopy23} alt='starcopy1'/>
+        <img className='starcopy24' src={starcopy24} alt='starcopy1'/>
+        <img className='starcopys6' src={starcopys6} alt='starcopy1'/>
+        <img className='starcopy25' src={starcopy25} alt='starcopy1'/>
+        <img className='starcopy26' src={starcopy26} alt='starcopy1'/>
+        <img className='starcopy27' src={starcopy27} alt='starcopy1'/>
+        <img className='starcopy28' src={starcopy28} alt='starcopy1'/>
+        <img className='starcopys7' src={starcopys7} alt='starcopy1'/>
+        <img className='starcopy29' src={starcopy29} alt='starcopy1'/>
+        <img className='starcopy30' src={starcopy30} alt='starcopy1'/>
+        <img className='starcopy31' src={starcopy31} alt='starcopy1'/>
+        <img className='starcopy32' src={starcopy32} alt='starcopy1'/>
+        <img className='starcopys8' src={starcopys8} alt='starcopy1'/>
+
+        <div className='n5'>$299,43</div>
+        <div className='f5'>$534,33</div>
+        <div className='o5'>24% Off</div>
+
+        <div className='n6'>$299,43</div>
+        <div className='f6'>$534,33</div>
+        <div className='o6'>24% Off</div>
+
+        <div className='n7'>$299,43</div>
+        <div className='f7'>$534,33</div>
+        <div className='o7'>24% Off</div>
+
+        <div className='n8'>$299,43</div>
+        <div className='f8'>$534,33</div>
+        <div className='o8'>24% Off</div>
+
+        <div className='loadmore'>LOAD MORE</div>
+        <img className='rectangle14' src={rectangle14} alt='rectangle14'/>
+
+        <img className='rectangle1copy10' src={rectangle1copy10} alt='rectangle1copy10'/>
+        <img className='shoe9'src={shoe9} alt='shoe9'/>
+        <div className='adidas'>Adidas Men Running Sneakers</div>
+        <div className='perform'>Performance and design. Taken right to the edge.</div>
+        <div className='shopnow'>SHOP NOW</div>
+       <img  className='whiteline'src={whiteline} alt='whiteline'/>
+
+       <img  className='shipping'src={shipping} alt='shipping'/>
+       <img  className='path1'src={path1} alt='path1'/>
+       <img  className='path2'src={path2} alt='path2'/>
+       <img  className='path3'src={path3} alt='path3'/>
+       <img  className='path4'src={path4} alt='path4'/>
+
+       <div className='free'>FREE SHIPPING</div>
+       <div className='refund'>100% REFUND</div>
+       <div className='support'>SUPPORT 24/7</div>
+
+       <div className='l1'>Lorem Ipsum is simply dummy text of the <br/>printing and typesetting <br/>industry.</div>
+       <div className='l2'>Lorem Ipsum is simply dummy text of the <br/>printing and typesetting <br/>industry.</div>
+       <div className='l3'>Lorem Ipsum is simply dummy text of the<br/> printing and typesetting <br/>industry.</div>
+      <div className='latest'>LATEST NEWS</div>
+ 
+      <img  className='emblem'src={emblem} alt='emblem'/>
+      <img  className='figma'src={figma} alt='figma'/>
+      <img  className='kronos'src={kronos} alt='kronos'/>
+
+      <div className='jan1'>01 Jan, 2015</div>
+      <div className='jan2'>01 Jan, 2015</div>
+      <div className='jan3'>01 Jan, 2015</div>
+      <div className='fashion'>Fashion Industry</div>
+      <div className='best'>Best Design Tools</div>
+      <div className='hr'>HR Community</div>
+      <div className='l4'>Lorem Ipsum is simply dummy text of the<br/> printing and typesetting <br/>industry.</div>
+      <div className='l5'>Lorem Ipsum is simply dummy text of the <br/>printing and typesetting <br/>industry.</div>
+      <div className='l6'>Lorem Ipsum is simply dummy text of the<br/> printing and typesetting <br/>industry.</div>
+
+      <div className='feature'>FEATURED PRODUCTS</div>
+
+      <img className='shoe10'src={shoe10} alt='shoe10'/>
+      <img className='shoe11'src={shoe11} alt='shoe11'/>
+      <img className='shoe12'src={shoe12} alt='shoe12'/>
+
+      <div className='blue1'>Blue Swade Nike<br/> Sneakers</div>
+      <div className='blue2'>Blue Swade Nike<br/> Sneakers</div>
+      <div className='blue3'>Blue Swade Nike <br/>Sneakers</div>
+
+      <img className='starcopy33' src={starcopy33} alt='starcopy1'/>
+        <img className='starcopy34' src={starcopy34} alt='starcopy1'/>
+        <img className='starcopy35' src={starcopy35} alt='starcopy1'/>
+        <img className='starcopy36' src={starcopy36} alt='starcopy1'/>
+        <img className='starcopys9' src={starcopys9} alt='starcopy1'/>
+        <img className='starcopy37' src={starcopy37} alt='starcopy1'/>
+        <img className='starcopy38' src={starcopy38} alt='starcopy1'/>
+        <img className='starcopy39' src={starcopy39} alt='starcopy1'/>
+        <img className='starcopy40' src={starcopy40} alt='starcopy1'/>
+        <img className='starcopys10' src={starcopys10} alt='starcopy1'/>
+        <img className='starcopy41' src={starcopy41} alt='starcopy1'/>
+        <img className='starcopy42' src={starcopy42} alt='starcopy1'/>
+        <img className='starcopy43' src={starcopy43} alt='starcopy1'/>
+        <img className='starcopy44' src={starcopy44} alt='starcopy1'/>
+        <img className='starcopys11' src={starcopys11} alt='starcopy1'/>
+
+        <div className='d1'>$499</div>
+        <div className='d2'>$499</div>
+        <div className='d3'>$499</div>
+        <div className='b1'>$599</div>
+        <div className='b2'>$599</div>
+        <div className='b3'>$599</div>
+        <img className='line1'src={line1} alt='line1'/>
+        <img className='line2'src={line2} alt='line1'/>
+        <img className='line3'src={line3} alt='line1'/>
+        
+        <img className='bluebox'src={bluebox} alt='bluebox'/>
+        <img className='roundblue'src={roundblue} alt='roundblue'/>
+        <div className='Search1'>Search query...</div>
+        <div className='Search2'>Search</div>
+        
+        <img className='rectangle437' src={rectangle437} alt='rectangle437'/>
+        <img className='rectangle3831' src={rectangle3831} alt='rectangle3831'/>
+        <img className='vector401' src={vector401} alt='vector401'/>
+        <div className='comm1'>E-Comm</div>
+        <div className='us1'>Follow Us</div>
+        <div className='us2'>Contact Us</div>
+        <div className='i1'>Lorem Ipsum is simply dummy text of the printing and 
+        typesetting industry. Lorem Ipsum has been the industry's standard dummy
+         text ever.Since the 1500s, when<br/> an unknown printer.</div>
+        <div className='i2'>Since the 1500s, when an unknown printer
+         took a galley of type and scrambled.</div>
+        <div className='i3'>E-Comm , 4578 <br/>Marmora Road,<br/> Glasgow D04 89GR</div>
+
+        <img className='facebook'src={facebook} alt='facebook'/>
+        <img className='twitter'src={twitter} alt='twitter'/>
+
+        <div className='info1'>Infomation</div>
+        <div className='service1'>Service</div>
+        <div className='account1'>My Account</div>
+        <div className='our'>Our Offers</div>
+        <div className='us3'>About Us<br/>
+Infomation <br/>
+Privacy Policy<br/>
+Terms & Conditions</div>
+        <div className='us4'>About Us<br/>
+Infomation <br/>
+Privacy Policy<br/>
+Terms & Conditions</div>
+        <div className='us5'>About Us<br/>
+Infomation <br/>
+Privacy Policy<br/>
+Terms & Conditions</div>
+        <div className='us6'>
+        About Us<br/>
+Infomation <br/>
+Privacy Policy<br/>
+Terms & Conditions
+        </div>
+        <img className='whitelength'src={whitelength} alt='whitelength'/>
+        <div className='theme'>© 2018 Ecommerce theme by www.bisenbaev.com</div>
+
+        <img className='Western_union'src={Western_union} alt='Western_union'/>
+        <img className='Group19'src={Group19} alt='Group19'/>
+        <img className='Paypal'src={Paypal} alt='Paypal'/>
+        <img className='visa1'src={visa1} alt='visa1'/>
+        <img className='visa2'src={visa2} alt='visa2'/>
+        <img className='visa3'src={visa3} alt='visa3'/>        
+
+
+    </div>
+  )
+}
+
+export default Component;
